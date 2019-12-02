96ee989ce6cda93fca34438e11467c35b37e2f63
maitreebain <maitreebain@gmail.com>
2019-12-02 00:26:21 -0500

    can't call class, work on it

---
+++ SongsTableViewSearchBar/SongViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SongViewController.swift
+//  SongsTableViewSearchBar
+//
+//  Created by Maitree Bain on 12/2/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class SongViewController: UIViewController {
+
+    var song = [Song]() {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
